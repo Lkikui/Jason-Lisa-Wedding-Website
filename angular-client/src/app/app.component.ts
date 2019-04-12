996510084dfb3e87ea58d958b7b3fee35d6a891b
LisaRyland <lisa.ryland@gmail.com>
2019-04-11 17:38:01 -0700

    changed website design, added burlap and lace

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { discoverLocalRefs } from '@angular/core/src/render3/context_discovery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'My First Angular App!';
+  
+  ngOnInit() {
+    this.adjustDivWidth();
+    window.onresize = this.adjustDivWidth;
+  }
+  
+  adjustDivWidth() {
+    var wrapperDiv = $(".wrapper")[0] as HTMLCanvasElement;
+    var innerDiv = $(".mainContents")[0] as HTMLCanvasElement;
+
+    innerDiv.style.width = `${wrapperDiv.offsetWidth - 43 - 65 }px`;
+  }
+
 }
